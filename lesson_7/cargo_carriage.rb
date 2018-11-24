5d22ec913ee710e826bda10995ef907932ea568e
umeca90 <umeca90@gmail.com>
2018-11-24 15:29:10 +0300

    pushing tasks for lesson 'Blocks proc and lambda'

--- lesson_7/cargo_carriage.rb
+++ lesson_7/cargo_carriage.rb
@@ -3,9 +3,9 @@ class CargoCarriage < Carriage
   def initialize(space, car_num)
     super
   end
-  
+
   def take_space(space)
-   super(space) unless space + @space_taken > @space
+    super(space) unless space + @space_taken > @space
   end
 
 end
--- lesson_7/carriage.rb
+++ lesson_7/carriage.rb
@@ -1,13 +1,13 @@
 class Carriage
   include CompanyName
   include Validator
-  
+
   CAR_REGEXP = /(^[a-zA-Z]{2}-?[0-9]{1}$)|(^[а-яА-Я]{2}-?[0-9]{1}$)/
-  
+
   attr_reader :type, :space, :space_taken, :car_num
-  
+
   @@cars = []
-  
+
   def initialize(car_num, space)
     @type = self.class
     @space = space
@@ -16,17 +16,17 @@ class Carriage
     @@cars << self
     validate!
   end
-  
+
   def take_space(space)
     @space_taken += space
   end
-  
+
   def space_available
     @space - @space_taken
-  end  
-  
+  end
+
   protected
-  
+
   def validate!
     raise "Неверный формат номера" if car_num !~ CAR_REGEXP
     raise "Свободное место должно быть указано в цифрах" if self.space == 0
